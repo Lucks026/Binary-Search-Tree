740fa8f665b671c640ec6e44dbaee52e3670b9e8
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-15 09:28:00 -0300

    add: search returns node pointer

--- src/bst.c
+++ src/bst.c
@@ -15,6 +15,13 @@ Node *bst_insert(Node *root, int value) {
         root->left = bst_insert(root->left, value);
     else if (value > root->data)
         root->right = bst_insert(root->right, value);
-    // duplicates ignored
     return root;
+}
+
+Node *bst_search(Node *root, int value) {
+    if (root == NULL || root->data == value)
+        return root;
+    if (value < root->data)
+        return bst_search(root->left, value);
+    return bst_search(root->right, value);
 }
